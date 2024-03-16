f8f5b9ac9266a498ab75c82b2b960d0a9372b6fa
minthaj972 <120191720+minthaj972@users.noreply.github.com>
2024-03-16 23:44:59 +0530

    Add files via upload

---
+++ static/scripts.js
@@ -0,0 +1,27 @@
+const scrollers = document.querySelectorAll('.scroller');
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
+    addAnimation();
+}
+
+function addAnimation(){
+    scrollers.forEach((scroller)=>{
+        scroller.setAttribute("data-animated", true);
+
+        const scrollerInner = scroller.querySelector('.pic-container');
+        const scrollerContent= Array.from(scrollerInner.children)
+        
+        scrollerContent.forEach((item) =>{
+        const duplicatedItem = item.cloneNode(true);
+        duplicatedItem.setAttribute("aria-hidden", true);
+        scrollerInner.appendChild(duplicatedItem);
+        });    })
+}
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = "flex";
+    console.log("hi"); 
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = "none"; 
+}
